c2c79e3578adf50f70a4045072de7eb0ab58b56b
Vitor Barbosa <vitorbarbosa789@gmail.com>
2021-07-03 12:37:15 -0300

    Remoção da lib enjoyhint

--- Aplication/app/js/Storage.js
+++
@@ -1,9 +0,0 @@
-const Storage = {
-    get() {
-      return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
-    },
-    set(transactions) {
-      localStorage.setItem("dev.finances:transactions",
-        JSON.stringify(transactions))
-    }
-  }
